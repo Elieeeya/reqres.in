c1f557ffd0a86e8db34c112eade36d116a68218f
Elieeeya <ik.slydnev@gmail.com>
2023-01-22 18:58:16 +0300

    update reqres.in get requests

--- test/test_get_list_resource.py
+++ test/test_get_list_resource.py
@@ -11,4 +11,3 @@ def test_get_list_resource_response_page_1_data():
     assert response.json()['data'][1]['name'] == 'fuchsia rose'
     assert response.json()['data'][2]['year'] == 2002
     assert response.json()['data'][3]['color'] == '#7BC4C4'
-    assert response.json()['data'][4]['pantone_value'] == '17-1456'
